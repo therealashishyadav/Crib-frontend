c7a0e66a1dd7284b9055c55375d04bf7d44a9b58
Ashish Yadav <ashishyadav01111998@gmail.com>
2026-07-17 16:54:06 +0530

    new commit 8

--- src/service/inquiry.service.ts
+++ src/service/inquiry.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Inquiry } from '../entity/Inquiry';
 import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +14,12 @@ export class InquiryService {
   postInquiry(inquiry: Inquiry) {
     return this.http.post(`${environment.apiUrl}/inquiry/sendMessage`, inquiry);
   }
+
+getMyInquiries(): Observable<Inquiry[]> {
+  return this.http.get<Inquiry[]>(`${environment.apiUrl}/api/inquiries/my`);
+}
+
+getReceivedInquiries(): Observable<Inquiry[]> {
+  return this.http.get<Inquiry[]>(`${environment.apiUrl}/api/inquiries/received`);
+}
 }
